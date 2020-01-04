b02cc0c9a2e47aba46f50a05589d945057567b45
A <s5935512030@phuket.psu.ac.th>
2020-01-04 19:59:00 +0700

    asd

--- index.js
+++ index.js
@@ -35,7 +35,9 @@ app.use(
 
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://psu-topgun2020.netlify.com/')
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.setHeader('Access-Control-Allow-Origin', 'https://psu-topgun2020.netlify.com')
+  res.setHeader('Access-Control-Allow-Origin', 'http://35.194.179.37:3000')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
   res.setHeader('Access-Control-Allow-Credentials', true)
